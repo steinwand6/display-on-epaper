68f493d6d213fb4ea94c0f5de4b343caca1e0a60
tomoya ishigaki <steinwand.tomo@gmail.com>
2023-03-11 13:36:18 +0900

    change to display DONE subtasks in TODO maintask

--- src/todos.rs
+++ src/todos.rs
@@ -3,14 +3,36 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+use regex::Regex;
+
 pub fn get_todos(task_file_path: &str) -> Vec<String> {
     let mut tasks = Vec::new();
     let task_list = File::open(task_file_path).unwrap();
     let reader = BufReader::new(task_list);
+
+    let re_first_level = Regex::new(r"^\* ").unwrap();
+    let re_maintask = Regex::new(r"^\*{2} TODO").unwrap();
+    let re_subtask_todo = Regex::new(r"^\*\*\*+ TODO").unwrap();
+    let re_subtask_done = Regex::new(r"^\*\*\*+ DONE").unwrap();
+    let mut in_project = false;
     for line in reader.lines() {
-        let line = line.unwrap();
-        if line.contains(" TODO ") {
-            let line = line.replace(" TODO ", "").replace("*", " ");
+        let mut line = line.unwrap();
+
+        if re_first_level.is_match(&line) {
+            in_project = line.contains("Projects");
+        }
+        if !in_project {
+            continue;
+        }
+
+        if re_maintask.is_match(&line) {
+            line = line.replace("** TODO ", "- ");
+            tasks.push(line);
+        } else if re_subtask_todo.is_match(&line) {
+            line = line.replace(" TODO ", "[ ]").replace("*", " ");
+            tasks.push(line);
+        } else if re_subtask_done.is_match(&line) {
+            line = line.replace(" DONE ", "[x]").replace("*", " ");
             tasks.push(line);
         }
     }
